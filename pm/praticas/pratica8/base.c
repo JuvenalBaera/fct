4a7d560e5e33b6f1541d52bcdb7b70d687b6200a
JuvenalBaera <juvenalbaera15@gmail.com>
2022-11-18 12:23:48 +0000

    Update pratica8 folder

--- pm/praticas/pratica8/base.c
+++ pm/praticas/pratica8/base.c
@@ -66,7 +66,7 @@ void le_inteiro(int *num, int min, int max, char *msg){
         printf("%s", msg);
         ret = scanf("%d", num);
         clear_buffer();
-    }while(*num < min || *num > max);
+    }while(ret != 1 || (*num < min || *num > max));
 }
 
 void le_text(char *text, int tam){
@@ -76,7 +76,7 @@ void le_text(char *text, int tam){
     for(i = 0; i < tam; i++){
         ch = getchar();
         if(ch == '\n'){
-            break;;
+            break;
         }
         else
             text[i] = ch;
@@ -88,7 +88,17 @@ void tira_enter(char txt[]){
     txt[strlen(txt)-1] = ' ';
 }
 
+
 // #####################################################
+
+int verifica_nss(BASEDADOS base, int nss){
+    for(int i = 0; i < MAX_CIDADAO; i++){
+        if(nss == base.cidadaos[i].nss.num_serie)
+            return 1;
+    }
+    return 0;
+}
+
 void le_nss(struct NUM_SERV_SEC *nss, BASEDADOS base){
     int controlo = 1;
     do{
@@ -96,7 +106,7 @@ void le_nss(struct NUM_SERV_SEC *nss, BASEDADOS base){
         le_inteiro(&(nss->num_serie), 1, 30000, "Digite o NSS: ");
         for(int i = 0; i < TAM_DADOS; i++){
             if(nss->num_serie == base.cidadaos[i].nss.num_serie){
-                printf("\nNUMERO DE SERIE JA EXISTE\n");
+                printf("\nNÚMERO DE SÉRIE JÁ EXISTE\n");
                 controlo = 0;
                 break;
             }
@@ -111,7 +121,7 @@ void le_info_pessoal(struct INFO_PESSOAL *infp){
     printf("\tNome.........: ");
     le_text(infp->nome, TAM_DADOS);
     le_inteiro(&(infp->dia_nasc), 1, 31, "\tDia..........: ");
-    le_inteiro(&(infp->mes_nasc), 1, 12, "\tMes..........: ");
+    le_inteiro(&(infp->mes_nasc), 1, 12, "\tMês..........: ");
     le_inteiro(&(infp->ano_nasc), 1900, 2022, "\tAno..........: ");
     le_inteiro(&(infp->altura), 1, 270, "\tAltura.......: ");
     printf("\tCor dos olhos: ");
@@ -121,9 +131,9 @@ void le_info_pessoal(struct INFO_PESSOAL *infp){
 }
 
 void le_info_number(struct INFO_NUMBER *infn){
-    printf("INTRODUZA DOS NUMEROS\n");
-    le_inteiro(&(infn->bi), 1, 30000, "\tNumero de BI......: ");
-    le_inteiro(&(infn->contribuinte), 1, 30000, "\tnr de contribuente: ");
+    printf("INTRODUZA OS DADOS NUMÉRICOS\n");
+    le_inteiro(&(infn->bi), 1, 30000, "\tNúmero de BI......: ");
+    le_inteiro(&(infn->contribuinte), 1, 30000, "\tNr de contribuente: ");
     le_inteiro(&(infn->seg_social), 1, 30000, "\tSeg social........: ");
     le_inteiro(&(infn->carta_cond), 1, 30000, "\tNr da carta_cond..: ");
 }
@@ -132,9 +142,9 @@ void le_morada(struct MORADA *address){
     printf("INTRODUZA OS DADOS DA MORADA\n");
     printf("\tRua...........: ");
     le_text(address->rua, TAM_DADOS);
-    le_inteiro(&(address->cod_postal1), 1, 9999, "\tCodigo postal1: ");
-    le_inteiro(&(address->cod_postal2), 1, 9999, "\tCodigo postal2: ");
-    printf("\tCodigo postal3: ");
+    le_inteiro(&(address->cod_postal1), 1, 9999, "\tCódigo postal1: ");
+    le_inteiro(&(address->cod_postal2), 1, 9999, "\tCódigo postal2: ");
+    printf("\tCódigo postal3: ");
     le_text(address->cod_postal3, TAM_DADOS);
 }
 
@@ -162,7 +172,7 @@ void print_nss(struct NUM_SERV_SEC nss){
 }
 
 void print_info_pessoal(struct INFO_PESSOAL infp){
-    printf("INFORMACAO PESSOAL\n");
+    printf("INFORMAÇÃO PESSOAL\n");
     printf("\tNome......: %s\n", infp.nome);
     printf("\tData......: %02d/%02d/%04d\n", infp.dia_nasc, infp.mes_nasc, infp.ano_nasc);
     printf("\tAltura....: %d\n", infp.altura);
@@ -171,23 +181,23 @@ void print_info_pessoal(struct INFO_PESSOAL infp){
 }
 
 void print_info_number(struct INFO_NUMBER infn){
-    printf("INFORMACAO DE NUMERO\n");
+    printf("INFORMAÇÃO DE NÚMERO\n");
     printf("\tBI...............: %d\n", infn.bi);
     printf("\tNr. Contribuente.: %d\n", infn.contribuinte);
     printf("\tSeguranca Social.: %d\n", infn.seg_social);
-    printf("\tCarta de conducao: %d\n", infn.carta_cond);
+    printf("\tCarta de condução: %d\n", infn.carta_cond);
 }
 
 void print_morada(struct MORADA address){
-    printf("INFORMACAO DE MORADA\n");
+    printf("INFORMAÇÃO DE MORADA\n");
     printf("\tRua...........: %s\n", address.rua);
-    printf("\tCodigo Postal1: %d\n", address.cod_postal1);
-    printf("\tCodigo Postal2: %d\n", address.cod_postal2);
-    printf("\tCodigo Postal3: %s\n", address.cod_postal3);
+    printf("\tCódigo Postal1: %d\n", address.cod_postal1);
+    printf("\tCódigo Postal2: %d\n", address.cod_postal2);
+    printf("\tCódigo Postal3: %s\n", address.cod_postal3);
 }
 
 void print_confidencial(struct CONFID clubes){
-    printf("INFORMACAO CONFIDENCIAL\n");
+    printf("INFORMAÇÃO CONFIDENCIAL\n");
     printf("\tPartido: %s\n", clubes.partido);
     printf("\tClube..: %s\n", clubes.clube);
 }
@@ -211,7 +221,11 @@ void incializar_base(BASEDADOS *base){
 }
 
 void base_vazia(){
-    printf("\nBASE DE DADOS ESTA VAZIA\n\n");
+    printf("\nBASE DE DADOS ESTÁ VAZIA\n\n");
+}
+
+void nao_existe_nss(int nss){
+    printf("\nCIDADÃO COM NSS %d NÃO EXISTE\n", nss);
 }
 
 void mostrar_nss(BASEDADOS base){
@@ -233,7 +247,7 @@ void mostrar_info_cidadao(BASEDADOS base, int nss){
             return;
         }
     }
-    printf("\nO Cidadao com NSS %d Nao existe\n", nss);
+    nao_existe_nss(nss);
 }
 
 void modificar_cidadao(BASEDADOS *base, int nss){
@@ -246,7 +260,7 @@ void modificar_cidadao(BASEDADOS *base, int nss){
             return;
         }
     }
-    printf("\nCidadao com NSS %d nao existe\n", nss);
+    nao_existe_nss(nss);
 }
 
 void novo_registo(BASEDADOS *base){
@@ -260,29 +274,27 @@ void novo_registo(BASEDADOS *base){
         }
     }
     else
-        printf("\nA BASE DE DADOS ESTA CHEIA\n");
+        printf("\nA BASE DE DADOS ESTÁ CHEIA\n");
 }
 
-void apagar_registo(BASEDADOS *base, int nss){
+int apagar_registo(BASEDADOS *base, int nss){
     for(int i = 0; i < MAX_CIDADAO; i++){
         if(base->cidadaos[i].nss.num_serie == nss){
             base->cidadaos[i].nss.num_serie = 0;
             base->tamanho--;
-            return;
+            return 1;
         }
     }
-    printf("\nCidadao com NSS %d, nao existe\n\n", nss);
-}
-
-int verifica_nss(BASEDADOS base, int nss){
-    for(int i = 0; i < MAX_CIDADAO; i++){
-        if(nss == base.cidadaos[i].nss.num_serie)
-            return 1;
-    }
+    nao_existe_nss(nss);
     return 0;
 }
 
+
 // #################### ARQUIVOS ##########################
+
+void erro_abrir_fich(char arq[]){
+    printf("\nERRP AO ABRIR O ARQUIVO %s\n", arq);
+}
 
 void inserir_cid_ficheiro(BASEDADOS *base, struct CIDADAO cidadao){
     if(base->tamanho < MAX_CIDADAO){
@@ -290,7 +302,7 @@ void inserir_cid_ficheiro(BASEDADOS *base, struct CIDADAO cidadao){
             base->cidadaos[base->tamanho++] = cidadao;
         }
         else
-            printf("\nCIDADAO COM NSS %d EXISTE\n", cidadao.nss.num_serie);
+            printf("\nCIDADÃO COM NSS %d JÁ EXISTE\n", cidadao.nss.num_serie);
     }
     else
         printf("\nA BASE DE DADOS ESTA CHEIA\n");
@@ -305,9 +317,10 @@ void ler_binario(BASEDADOS *base, char arq[]){
             fread(&cid_aux, sizeof(struct CIDADAO), 1, file);
             inserir_cid_ficheiro(base, cid_aux);
         }
+        fclose(file);
     }
     else
-        printf("\nErro ao abrir o arquivo %s\n", arq);
+        erro_abrir_fich(arq);
 }
 
 void ler_texto(BASEDADOS *base, char arq[]){
@@ -316,7 +329,7 @@ void ler_texto(BASEDADOS *base, char arq[]){
 
     if(file){
         while(!feof(file)){
-            fscanf(file, "%d\n", &cid_aux.nss);
+            fscanf(file, "%d\n", &cid_aux.nss.num_serie);
 
             fgets(cid_aux.infp.nome, TAM_DADOS, file);
             tira_enter(cid_aux.infp.nome);
@@ -348,9 +361,10 @@ void ler_texto(BASEDADOS *base, char arq[]){
 
             inserir_cid_ficheiro(base, cid_aux);
         }
+        fclose(file);
     }
     else
-        printf("\nErro ao abrir o arquivo %s\n", arq);
+        erro_abrir_fich(arq);
 }
 
 void escrever_binario(BASEDADOS base, char arq[]){
@@ -361,9 +375,10 @@ void escrever_binario(BASEDADOS base, char arq[]){
                 fwrite(&(base.cidadaos[i]), sizeof(struct CIDADAO), 1, file);
             }
         }
+        fclose(file);
     }
     else
-        printf("\nEERRO AO ABRIR O ARQUIVO %s\n", arq);
+        erro_abrir_fich(arq);
 }
 
 void escrever_texto(BASEDADOS base, char arq[]){
@@ -397,35 +412,104 @@ void escrever_texto(BASEDADOS base, char arq[]){
                 fprintf(file, "%s\n", base.cidadaos[i].clubes.clube);
             }
         }
+        fclose(file);
     }
     else
-        printf("\nEERRO AO ABRIR O ARQUIVO %s\n", arq);
+        erro_abrir_fich(arq);
+}
+
+
+// ########################### inserção < ###################
+
+void ler_stdin_fich(BASEDADOS *base){
+
+    struct CIDADAO cid;
+    
+    for(int i = 0; i < 5; i++){
+        scanf("%d\n", &cid.nss.num_serie);
+        le_text(cid.infp.nome, TAM_DADOS);
+        scanf("%d\n", &cid.infp.dia_nasc);
+        scanf("%d\n", &cid.infp.mes_nasc);
+        scanf("%d\n", &cid.infp.ano_nasc);
+        scanf("%d\n", &cid.infp.altura);
+        le_text(cid.infp.olhos, TAM_DADOS);
+        le_text(cid.infp.cabelo, TAM_DADOS);
+
+        scanf("%d\n", &cid.infn.bi);
+        scanf("%d\n", &cid.infn.contribuinte);
+        scanf("%d\n", &cid.infn.seg_social);
+        scanf("%d\n", &cid.infn.carta_cond);
+
+        le_text(cid.address.rua, TAM_DADOS);
+        scanf("%d\n", &cid.address.cod_postal1);
+        scanf("%d\n", &cid.address.cod_postal2);
+        le_text(cid.address.cod_postal3, TAM_DADOS);
+
+        le_text(cid.clubes.partido, TAM_DADOS);
+        le_text(cid.clubes.clube, TAM_DADOS);
+
+        print_cidadao(cid);
+        base->cidadaos[i] = cid;
+    }
+    escrever_binario(*base, "resultado.dat");
+    exit(1);
+}
+
+// ################## EXERCÍCIO 4.4 ###################3
+void mundo_verde(BASEDADOS *base, char arq[], char esc[]){
+    ler_texto(base, arq);
+    for(int i = 0; i < MAX_CIDADAO; i++){
+        if(base->cidadaos[i].nss.num_serie != 0){
+            strcpy(base->cidadaos[i].clubes.clube, "Sporting Clube de Portugal");
+        }
+    }
+    escrever_texto(*base, esc);
+}
+
+
+void isolar_cidadao(){
+    BASEDADOS aux_base, base;
+    incializar_base(&aux_base);
+    incializar_base(&base);
+    ler_texto(&aux_base, "cobaia.txt");
+    
+    for(int i = 0; i < MAX_CIDADAO; i++){
+        if(aux_base.cidadaos[i].infp.altura >= 160){
+            base.cidadaos[i] = aux_base.cidadaos[i];
+            base.tamanho++;
+        }
+    }
+
+    escrever_texto(base, "maior_160cm.txt");
 }
 
 
 // ########################## MENU ##########################
 void menu(char *opcao){
     printf("---------------------------------------------------------------------------\n");
-    printf("|   /*********** PROGRAMA SIRP - Registo dos cidadaos **********/          |\n");
+    printf("|   /*********** PROGRAMA SIRP - Registo dos cidadãos **********/          |\n");
     printf("----------------------------------------------------------------------------\n");
     printf("|                                                                          |\n");
-    printf("| a - Mostrar os numero de serie de todos os elementos da base de dados    |\n");
-    printf("| b - Mostrar toda a informacao do cidadao com um certo numero (a pedir)   |\n");
-    printf("| c - Modificar a informacao do cidadao com um certo numero (a pedir)      |\n");
-    printf("| d - Criar um novo registo para um novo cidadao                           |\n");
-    printf("| e - Apagar o registo com um certo numero (a pedir)                       |\n");
-    printf("| f - Ler a base de dados de um ficheiro em modo binario (nome a pedir)    |\n");
+    printf("| a - Mostrar os números de série de todos os elementos da base de dados   |\n");
+    printf("| b - Mostrar toda a informação do cidadão com um certo número (a pedir)   |\n");
+    printf("| c - Modificar a informação do cidadão com um certo número (a pedir)      |\n");
+    printf("| d - Criar um novo registo para um novo cidadão                           |\n");
+    printf("| e - Apagar o registo com um certo número (a pedir)                       |\n");
+    printf("| f - Ler a base de dados de um ficheiro em modo binário (nome a pedir)    |\n");
     printf("| g - Ler a base de dados de um ficheiro em modo de texto (nome a pedir)   |\n");
-    printf("| h - Escrever a base de dados num ficheiro em modo binario (nome a pedir) |\n");
+    printf("| h - Escrever a base de dados num ficheiro em modo binário (nome a pedir) |\n");
     printf("| i - Escrever a base de dados num ficheiro em modo de text (nome a pedir) |\n");
+    printf("| j - Tornar o mundo verde                                                 |\n");
+    printf("| k - Isolar cidadãos                                                      |\n");
     printf("| s - Sair do programa                                                     |\n");
     printf("| z - Mostrar todos (EXTRA)                                                |\n");
     printf("|                                                                          |\n");
     printf("----------------------------------------------------------------------------\n");
 
-    printf("Qual e a opcao: ");
+    printf("Qual é a opção: ");
     *opcao = getchar();
     clear_buffer();
+    printf("\n");
 }
 
 
@@ -435,16 +519,21 @@ int main(int argc, char *argv[]){
     BASEDADOS basedados;
     int nss;
     char opcao;
-    char arquivo[TAM_DADOS];
+    char arquivo[TAM_DADOS], arq_ex_4_4[TAM_DADOS];
 
-    system("cls");
+    system("clear");
 
     incializar_base(&basedados);
-
+    
     if(argc == 2){
-        printf("%s\n", strchr(argv[1], '.'));
-        // ler_texto(&basedados, argv[1]);
+        if(strcmp(strrchr(argv[1], '.'),  ".dat") == 0){
+            ler_binario(&basedados, argv[1]);
+        }
+        else if(strcmp(strrchr(argv[1], '.'), ".txt") == 0){
+            ler_texto(&basedados, argv[1]);
+        }
     }
+
     do{
         menu(&opcao);
         switch(opcao){
@@ -480,8 +569,8 @@ int main(int argc, char *argv[]){
             case 'e': case 'E':
                 if(basedados.tamanho > 0){
                     le_inteiro(&nss, 1, 30000, "Digite o NSS a Apagar: ");
-                    apagar_registo(&basedados, nss);
-                    printf("\nREGISTRO REMOVIDO COM SUCESSO\n\n");
+                    if(apagar_registo(&basedados, nss) == 1)
+                        printf("\nREGISTRO REMOVIDO COM SUCESSO\n\n");
                 }
                 else
                     base_vazia();
@@ -514,18 +603,31 @@ int main(int argc, char *argv[]){
                 else
                     base_vazia();
             break;
+            case 'j': case 'J':
+                printf("Digite o nome do arquivo a ler: ");
+                le_text(arquivo, TAM_DADOS);
+                printf("Nome do arquivo a escrever: ");
+                le_text(arq_ex_4_4, TAM_DADOS);
+                mundo_verde(&basedados, arquivo, arq_ex_4_4);
+            break;
+            case 'k': case 'K':
+                isolar_cidadao();
+            break;
+            case 'w': case 'W':
+                ler_stdin_fich(&basedados);
+            break;
             case 's': case 'S':
                 printf("\n|========= MUITO OBRIGADO ==========|\n");
                 exit(0);
             break;
             case 'z': case 'Z':
-                printf("\n");
+                printf("\nTAMANHO DA BASE: %d\n", basedados.tamanho);
                 for(int i = 0; i < MAX_CIDADAO; i++)
                     printf("NSS: %2d = %d\n", i, basedados.cidadaos[i].nss.num_serie);
                 printf("\n");
             break;
             default:
-                printf("\nOPCAO INVALIDA\n\n");
+                printf("\nOPÇÃO INVÁLIDA\n\n");
         }
     }while(1);
 
