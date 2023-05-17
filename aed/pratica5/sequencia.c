58f68337f13c875e7663186eb7a07b5e44a104a3
Dadchima <122903754+Dadchima@users.noreply.github.com>
2023-05-17 18:29:10 +0100

    Update sequencia.c

--- aed/pratica5/sequencia.c
+++ aed/pratica5/sequencia.c
@@ -16,7 +16,7 @@ sequencia criaSequencia(int cap){
         free(seq);
         return NULL;
     }
-    seq->capacidade = cap + 1;
+    seq->capacidade = cap;
     seq->numElementos = 0;
 }
 
@@ -29,32 +29,59 @@ int vaziaSequencia(sequencia s){
 }
 
 void * elementoPosSequencia(sequencia s, int i){
-    return s->elementos[i];
+    return s->elementos[i-1];
     // return s->elementos + i;
 }
 
 void adicionaPosSequencia(sequencia s, void * elem, int i){
-    if(i == 1)
+    if(i == s->numElementos)
         s->elementos[i] = elem;
-    else if(i == tamanhoSequencia(s)) // X 1 2 3 X 4 5
-        s->elementos[tamanhoSequencia(s)] = elem;
-    else{
-	for(int j = s->capacidade; j >= i; j--)
+    else if(i < s->numElementos){
+	  for(int j = s->numElementos; j > (i-1); j--)   
 		s->elementos[j] = s->elementos[j-1]
-	s->elementos[i] = elem;
+	s->elementos[i-1] = elem;  
+	s->numElementos += 1;
     }
-    void * tmp = elem;
+    void * tmp = elem; //tá aqui a fzr o q??? N foi utilizado
 }
 void * removePosSequencia(sequencia s, int i){
-    void * ele = s->elementos + i;
-    free(s->elementos + i);
-    return ele;
+    void * elem = s->elementos[i-1];
+    int j;
+    for(j=i; j < s->numElementos; j++)
+    	s->elementos[j-1] = s->elementos[j];
+    s->numElementos--;
+    return elem;
 }
-// iterador iteradorSequencia(sequencia s){}
+
+iterador iteradorSequencia(sequencia s){
+	int i;
+	void ** aux = malloc(sizeof(void*) * s->numElementos);
+	for(i=0; i < s->numElementos; i++)
+		aux[i] = s->elementos[i];
+	return criaIterador(aux, s->numElementos);
+}
+
 void destroiSequencia (sequencia s ){
-    for(int i = 0; i < s->capacidade ;i++)
-        removePosSequencia(s, i);
-        // free(s->capacidade + i);
-    free(s);
+  free(s->elementos);
+  free(s);
 }
-void destroiSeqEElems(sequencia s, void (*destroi)(void *)){}
+
+void destroiSeqEElems(sequencia s, void (*destroi)(void *)){
+int j;
+	for(j = 0;  j < s->numElementos; j++)
+		destroi(s->elementos[j]);
+free(s->elementos);
+free(s);
+}
+
+void incSequencia(sequencia s){
+	int i; 
+	void ** aux;
+	aux = malloc(sizeof(void *) * s->capacidade * 2)
+	for(i=0; i < s->numElementos; i++)
+		aux[i] = s->elementos[i];
+	s->capacidade = capacidade * 2;
+	free(s->elementos);
+	s->elementos = aux;
+	free(aux);	
+}
