90b88eba0388230f3fb7b43fd0004c0eea94a2bb
Juvenal Baera <92170141+JuvenalBaera@users.noreply.github.com>
2024-05-17 16:31:22 +0100

    idk

---
+++ mde/labworks/labwork2/prolog/comentario.pl
@@ -0,0 +1,27 @@
+% instalacao(potencia, tipo instalção, contador de energia)
+
+% ligacoes(inst_ori, inst_dest, condutor, comprimento, perdas (%))
+%
+% condutor(tipo[rigido, multifilar], material[cobre, aluminio],isolamento[PVC, polietileno reticulado])
+%
+% dispositivo(id, consumo).
+%
+%
+%
+% adicionar
+/*
+• Adicionar uma instalação à rede.
+• Adicionar dispositivos a uma instalação.
+• Adicionar ligação entre duas instalações.
+• Adicionar consumo a um dispositivo de uma instalação.
+• …
+• Alterar uma instalação da rede.
+• Alterar um dispositivo de uma instalação.
+• Alterar ligação entre duas instalações.
+• Alterar consumo de um dispositivo de uma instalação.
+• …
+• Remover instalação da rede.
+• Remover um dispositivo de uma instalação.
+• Remover ligação entre duas instalações.
+• Remover consume de um dispositivo
+*/
